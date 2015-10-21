63e3976d6d03f211259c67268667dcb21d12add5
jacobtc13 <9855432@student.swin.edu.au>
2015-10-21 23:17:48 +1100

    creates project and unit testing

---
+++ ExerciseClub/ExerciseClubTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ExerciseClubTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
