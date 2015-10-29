081dd104400582f1f7903b3f8724031a22284257
hugh <9989951@student.swin.edu.au>
2015-10-29 23:09:54 +1100

    reupload, making sure all files are staged properly

---
+++ ExerciseClub/ExerciseClub/Activity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseClub
+{
+    class Activity
+    {
+        private string _time;
+        //private Profile _profile;
+        private DateTime _dateOfActivity;
+
+        public string Name { get; private set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+
+        public Activity()
+        {
+
+        }
+
+        public Activity(string name, string time, DateTime dateOfActivity, string location, string description)
+        {
+            Name = name;
+            _time = time;
+            _dateOfActivity = dateOfActivity;
+            Location = location;
+            Description = description;
+        }
+
+        public string ToString
+        {
+            get
+            {
+                return Name + "," + _time + "," + _dateOfActivity.ToString("yyyMMdd") + "," + Location + "," + Description;
+            }
+        }
+
+        
+
+
+
+
+    }
+}
---
+++ ExerciseClub/ExerciseClubTest/ActivityTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using ExerciseClub;
+
+namespace ExerciseClubTest
+{
+    [TestFixture()]
+    public class ActivityTestClass
+    {
+        [Test()]
+        public void TestCreateNewActivity()
+        {
+            string testActivity = "Tennis";
+            string testTime = "7:30";
+            DateTime testDate = new DateTime(2000, 01, 01);
+            string testLocation = "Rodlaver Arena";
+            string testDescription = "TEST";
+            Activity myActivity = new Activity(testActivity, testTime, testDate, testLocation, testDescription);
+
+            StringAssert.AreEqualIgnoringCase(testActivity, myActivity.Name, "Name not the same");
+        }
+
+        [Test()]
+        public void TestCreateCustomActivity()
+        {
+            Activity activity = new Activity("Basketball", "11:00", DateTime(2015, 10, 27), "Hawthorn", "Play a game of basketball");
+
+            Assert.AreSame("Basketball", activity.Name);
+            Assert.AreSame("11:00", activity.Time);
+            Assert.AreSame(DateTime(2015, 10, 27), activity.Date);
+            Assert.AreSame("Hawthorn", activity.Location);
+            Assert.AreSame("Play a game of basketball", activity.Description);
+        }
+
+        [Test()]
+        public void TestEditActivity()
+        {
+            Activity activity = new Activity();
+
+            EditActivity(1, "Jog");
+
+            Assert.AreSame("Jog", activity.Name);
+        }
+
+        [Test()]
+        public void TestFindActivity()
+        {
+            User user = new User();
+            Activity activity = new Activity();
+
+            Assert.AreSame(activity, user.FindActivity("Run"));
+            Assert.AreSame(activity, user.FindActivity("Go for a 5k run"));
+        }
+    }
+}
